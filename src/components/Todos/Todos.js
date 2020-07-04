0738ac15cc3ee7289bc78bf7c7f9574f111fc4eb
Georgi Popov <popovgeorgi002@gmail.com>
2020-07-04 22:12:47 +0300

    Application data is now stored in firebase

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import TodoForm from './TodoForm';
 import Search from './Search';
@@ -7,11 +7,34 @@ import TodoList from './TodoList';
 const Todos = () => {
     const [userTasks, setUserTasks] = useState([]);
 
+    useEffect(() => {
+        fetch('https://my-todo-app-d3e3e.firebaseio.com/tasks.json').then(
+            response => response.json()
+        ).then(responseData => {
+            const loadedTasks = [];
+            for (const key in responseData) {
+                loadedTasks.push({
+                    id: key,
+                    title: responseData[key].title
+                });
+            }
+            setUserTasks(loadedTasks);
+        });
+    }, []);
+
     const addTaskHandler = task => {
-        setUserTasks(prevTasks => [
-            ...prevTasks,
-            { id: Math.random().toString(), ...task }
-        ]);
+        fetch('https://my-todo-app-d3e3e.firebaseio.com/tasks.json', {
+            method: 'POST',
+            body: JSON.stringify(task),
+            headers: { 'Content-Type': 'application/json' }
+        }).then(response => {
+            return response.json();
+        }).then(responseData => {
+            setUserTasks(prevTasks => [
+                ...prevTasks,
+                { id: responseData.name, ...task }
+            ]);
+        });
     };
 
     const removeTaskHandler = taskId => {
